af2a2ed7928c189450093d06d049842b47fbc51b
Stefano Fortunati <stefano.fortunati@telecom-sudparis.eu>
2026-09-03 10:17:57 +0200

    Update R_vdW_est_const_det_iter.m

--- R_estimator_for_the_shape_matrix/Real_data_case/R_vdW_estimator/R_vdW_est_const_det_iter.m
+++ R_estimator_for_the_shape_matrix/Real_data_case/R_vdW_estimator/R_vdW_est_const_det_iter.m
@@ -23,10 +23,11 @@ function [ V_VDW,  alpha_est] = R_vdW_est_const_det_iter( y, T, pert, iter)
 D_m = full(DuplicationM(m));
 E_m = full(EliminationM(m));
 
-% Estimation of alpha
-alpha_est= alpha_estimator_sub_vdW_const( y, T, pert, D_m, E_m);
-
 for i = 1:iter
+
+    % Estimation of alpha
+    alpha_est= alpha_estimator_sub_vdW_const( y, T, pert, D_m, E_m);
+    
     % Calculation of the Constraint Matrix U_V for the constraint S(Sigma) = det(Sigma)^(1/m)
     D_Sigma = m^(-1)*(det(T)^(1/m))*inv(T);
     U_V = null((E_m*D_Sigma(:)).');
